75d1fee9976033d6ca74d3b3bfb9cd586a56f8bf
Julien Villemejane <julien.villemejane@institutoptique.fr>
2025-08-31 22:49:27 +0200

    PreCompiled L432KC

---
+++ keil_projects/L432KC/hello-L432KC_pre/main.cpp
@@ -0,0 +1,23 @@
+/*  
+ *  Structure of a main file for embedded project @ LEnsE
+ *****************************************************************
+ *  Tested with Nucleo L476RG board / Mbed OS 6
+ *****************************************************************
+ *  IntNum+ProTIS / https://lense.institutoptique.fr/
+ *      Based on Mbed OS 6 example : mbed-os-example-blinky-baremetal
+ */
+
+#include "mbed.h"
+ 
+DigitalOut led_ld2(LED1);
+
+int main()
+{
+    printf("\tLEnsE - L476RG  v%lf\n", 1.0);
+
+    while (true)
+    {
+        led_ld2 = !led_ld2;
+				thread_sleep_for(300);	// 300 ms
+    }
+}
