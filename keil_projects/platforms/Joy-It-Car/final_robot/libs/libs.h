8ae0e1781ee3577312bbdeee3fcfb1df5a4bfa6f
Julien Villemejane <julien.villemejane@institutoptique.fr>
2025-12-08 17:41:48 +0100

    Update Keil Projects / Joy It Car - Final test

---
+++ keil_projects/platforms/Joy-It-Car/final_robot/libs/libs.h
@@ -0,0 +1,8 @@
+/*  
+ *  Libraries directory
+ *****************************************************************
+ *  Author : J. VILLEMEJANE / LEnsE - Creation 2025/01/28
+ *****************************************************************
+ *  LEnsE / https://lense.institutoptique.fr/
+ *      Based on Mbed OS 6 example : mbed-os-example-blinky-baremetal
+ */
